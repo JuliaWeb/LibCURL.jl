48d3f229f292fed2eb8040a6bf5aae76be1575a1
Amit Murthy <amit.murthy@gmail.com>
2014-03-25 12:01:36 +0530

    Int32 to Cint

--- src/LibCURL.jl
+++ src/LibCURL.jl
@@ -12,17 +12,27 @@ libcurl = :libcurl
 
 include("lC_curl_h.jl")
 
-@c CURLcode curl_easy_setopt (Ptr{CURL}, CURLoption, Int...) libcurl
-@c CURLcode curl_easy_setopt (Ptr{CURL}, CURLoption,Ptr{Void}...) libcurl
-@c CURLcode curl_easy_setopt (Ptr{CURL}, CURLoption,Ptr{Uint8}...) libcurl
+curl_easy_setopt(handle, opt, ptrval::Array) = ccall((:curl_easy_setopt, "libcurl"), CURLcode, (Ptr{CURL}, CURLoption, Ptr{Void}), handle, opt, pointer(ptrval))  
+curl_easy_setopt(handle, opt, ptrval::Integer) = ccall((:curl_easy_setopt, "libcurl"), CURLcode, (Ptr{CURL}, CURLoption, Clong), handle, opt, ptrval)  
+curl_easy_setopt{T}(handle, opt, ptrval::Ptr{T}) = ccall((:curl_easy_setopt, "libcurl"), CURLcode, (Ptr{CURL}, CURLoption, Ptr{T}), handle, opt, ptrval)  
+curl_easy_setopt(handle, opt, ptrval::String) = ccall((:curl_easy_setopt, "libcurl"), CURLcode, (Ptr{CURL}, CURLoption, Ptr{Uint8}), handle, opt, ptrval)  
+# @c CURLcode curl_easy_setopt (Ptr{CURL}, CURLoption, Clong...) libcurl
+# @c CURLcode curl_easy_setopt (Ptr{CURL}, CURLoption,Ptr{Void}...) libcurl
+# @c CURLcode curl_easy_setopt (Ptr{CURL}, CURLoption,Ptr{Uint8}...) libcurl
 
-@c CURLcode curl_multi_setopt (Ptr{CURL}, CURLoption, Int...) libcurl
-@c CURLcode curl_multi_setopt (Ptr{CURL}, CURLoption,Ptr{Void}...) libcurl
-@c CURLcode curl_multi_setopt (Ptr{CURL}, CURLoption,Ptr{Uint8}...) libcurl
+curl_multi_setopt(handle, opt, ptrval::Array) = ccall((:curl_multi_setopt, "libcurl"), CURLMcode, (Ptr{CURLM}, CURLMoption, Ptr{Void}), handle, opt, pointer(ptrval))  
+curl_multi_setopt(handle, opt, ptrval::Integer) = ccall((:curl_multi_setopt, "libcurl"), CURLMcode, (Ptr{CURLM}, CURLMoption, Clong), handle, opt, ptrval)  
+curl_multi_setopt{T}(handle, opt, ptrval::Ptr{T}) = ccall((:curl_multi_setopt, "libcurl"), CURLMcode, (Ptr{CURLM}, CURLMoption, Ptr{T}), handle, opt, ptrval)  
+curl_multi_setopt(handle, opt, ptrval::String) = ccall((:curl_multi_setopt, "libcurl"), CURLMcode, (Ptr{CURLM}, CURLMoption, Ptr{Uint8}), handle, opt, ptrval)  
+# @c CURLcode curl_multi_setopt (Ptr{CURL}, CURLoption, Clong...) libcurl
+# @c CURLcode curl_multi_setopt (Ptr{CURL}, CURLoption,Ptr{Void}...) libcurl
+# @c CURLcode curl_multi_setopt (Ptr{CURL}, CURLoption,Ptr{Uint8}...) libcurl
 
-@c CURLcode curl_easy_getinfo (Ptr{CURL}, CURLoption,Ptr{Int}...) libcurl
-@c CURLcode curl_easy_getinfo (Ptr{CURL}, CURLoption,Ptr{Float64}...) libcurl
-@c CURLcode curl_easy_getinfo (Ptr{CURL}, CURLoption,Ptr{Uint8}...) libcurl
+curl_easy_getinfo(handle, opt, ptrval::Array) = ccall((:curl_easy_getinfo, "libcurl"), CURLcode, (Ptr{CURL}, CURLoption, Ptr{Void}), handle, opt, pointer(ptrval))  
+curl_easy_getinfo(handle, opt, ptrval::String) = ccall((:curl_easy_getinfo, "libcurl"), CURLcode, (Ptr{CURL}, CURLoption, Ptr{Uint8}), handle, opt, ptrval)  
+# @c CURLcode curl_easy_getinfo (Ptr{CURL}, CURLoption,Ptr{Clong}...) libcurl
+# @c CURLcode curl_easy_getinfo (Ptr{CURL}, CURLoption,Ptr{Cdouble}...) libcurl
+# @c CURLcode curl_easy_getinfo (Ptr{CURL}, CURLoption,Ptr{Uint8}...) libcurl
 
 include("lC_defines_h.jl")
 
