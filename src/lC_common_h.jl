9a43b5d43f47d4b2f8c13d9161ec4212fc0e0423
Tony Kelman <tony@kelman.net>
2014-04-20 18:31:45 -0700

    Download and use libcurl from WinRPM on Windows

--- src/lC_common_h.jl
+++ src/lC_common_h.jl
@@ -1,7 +1,7 @@
 macro c(ret_type, func, arg_types, lib)
   local args_in = Any[ symbol(string('a',x)) for x in 1:length(arg_types.args) ]
   quote
-    $(esc(func))($(args_in...)) = ccall( ($(string(func)), $(Expr(:quote, lib)) ), $ret_type, $arg_types, $(args_in...) )
+    $(esc(func))($(args_in...)) = ccall( ($(string(func)), $(Expr(:quote, eval(lib))) ), $ret_type, $arg_types, $(args_in...) )
   end
 end
 
