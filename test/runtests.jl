d2e3b7029581e6e245d115d7ef6b364583f446de
Iain Dunning <iaindunning@gmail.com>
2015-08-13 14:15:43 -0400

    Add some sort of desperation testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,8 @@
 using LibCURL
+
+# For lack of anything better, just run HTTPClient's tests.
+# They should exercise a large number of LibCURL features.
+# Until HTTPClient is bumped, that requires using master of
+# HTTPClient. Careful running this test locally!
+Pkg.checkout("HTTPClient")
+Pkg.test("HTTPClient",coverage=true)
