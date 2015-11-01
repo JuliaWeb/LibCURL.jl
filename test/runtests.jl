d98519591540f1ddc0302309d6d355b2a1555790
Tony Kelman <tony@kelman.net>
2015-10-31 23:35:12 -0700

    use include instead of Pkg.test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,4 @@ using LibCURL
 
 # For lack of anything better, just run HTTPClient's tests.
 # They should exercise a large number of LibCURL features.
-Pkg.test("HTTPClient",coverage=true)
+include(Pkg.dir("HTTPClient","test","runtests.jl"))
