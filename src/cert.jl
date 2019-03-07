6a4bb3fdf439d8843a2d7c2ae3517f9f61df5fdb
Fernando Chorney <fernando.chorney@invenia.ca>
2019-03-07 14:36:52 -0600

    Add doctrings to new types/functions

--- src/cert.jl
+++ src/cert.jl
@@ -33,16 +33,35 @@ const cert_directories = [
     "/var/ssl/certs",
 ]
 
+"""
+    CACertFile
+
+The `CACertFile` type represents a path to a `.pem` or `.crt` file to be used as a
+Certificate Authority Certificate for SSL/TLS requests.
+"""
 struct CACertFile
     path::AbstractString
     CACertFile(path::AbstractString) = new(path)
 end
 
+"""
+    CACertPath
+
+The `CACertPath` type represents a path to a directory containing `.pem` or `.crt` files
+to be used as Certificate Authority Certificates for SSL/TLS requests.
+"""
 struct CACertPath
     path::AbstractString
     CACertPath(path::AbstractString) = new(path)
 end
 
+"""
+    find_system_cacert() -> Union{CACertFile, CACertPath, Nothing}
+
+Search the filesystem for common locations of installed Certificate Authority Certificates.
+Will return a `CACertFile` if a specific file is found, or a `CACertPath` if a specific
+directory contains certificates is found. Will return `nothing` if nothing is found.
+"""
 function find_system_cacert()
     # Check if a cert file exists
     for f in cert_files
@@ -66,10 +85,20 @@ function find_system_cacert()
     return nothing
 end
 
+"""
+    enable_cacert(handle, cert::CACertFile)
+
+Apply the `CACertFile` path to the `CURLOPT_CAINFO` argument of the passed in `curl` handle.
+"""
 function enable_cacert(handle, cert::CACertFile)
     curl_easy_setopt(handle, CURLOPT_CAINFO, cert.path)
 end
 
+"""
+    enable_cacert(handle, cert::CACertPath)
+
+Apply the `CACertPath` path to the `CURLOPT_CAPATH` argument of the passed in `curl` handle.
+"""
 function enable_cacert(handle, cert::CACertPath)
     curl_easy_setopt(handle, CURLOPT_CAPATH, cert.path)
 end
