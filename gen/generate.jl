38dae7b3d5f63e2c5c1e4e5fa3403a4952200a02
Gnimuc <qiyupei@gmail.com>
2021-05-18 16:06:05 +0900

    Upgrade the wrapper script and rework the pacakge for multi-platform support

--- gen/generate.jl
+++ gen/generate.jl
@@ -1,66 +1,35 @@
-using Clang.cindex
-using Clang.wrap_c
-using Printf
+using Pkg
+using Pkg.Artifacts
+using Clang.Generators
+using Clang.Generators.JLLEnvs
+using LibCURL_jll
 
-# Set these to correspond to your local filesystem's curl and clang include paths
-const CURL_PATH = "/usr/local/opt/curl/include/curl"
-const CLANG_INCLUDES = [
-    "/usr/local/opt/llvm/include",
-]
+cd(@__DIR__)
 
-const SRC_DIR = abspath(@__DIR__, "..", "src")
+artifact_toml = joinpath(dirname(Pkg.pathof(LibCURL_jll)), "..", "StdlibArtifacts.toml")
+artifact_dir = Pkg.Artifacts.ensure_artifact_installed("LibCURL", artifact_toml)
 
-headers = map(x -> joinpath(CURL_PATH, x), ["curl.h", "easy.h", "multi.h"])
-context = wrap_c.init(;
-    headers = headers,
-    clang_args = String[],
-    common_file = joinpath(SRC_DIR, "lC_common_h.jl"),
-    clang_includes = CLANG_INCLUDES,
-    clang_diagnostics = true,
-    header_wrapped = (top_header, cursor_header) -> in(cursor_header, headers),
-    header_library = header -> "libcurl",
-    header_outputfile = header -> joinpath(SRC_DIR, "lC_") * replace(basename(header), "." => "_") * ".jl",
-)
-context.options.wrap_structs = true
+include_dir = joinpath(artifact_dir, "include") |> normpath
+curl_h = joinpath(include_dir, "curl", "curl.h")
+@assert isfile(curl_h)
 
-begin
-    context.headers = [joinpath(CURL_PATH, "curl.h")]
-    run(context)
-end
+# mprintf_h = joinpath(include_dir, "curl", "mprintf.h")
+# stdcheaders_h = joinpath(include_dir, "curl", "stdcheaders.h")
 
-function write_constants(filename::AbstractString, startswith_identifier::AbstractString, exports_file)
-    open(filename, "r") do file
-        lines = split(read(file, String), "\n")
+options = load_options(joinpath(@__DIR__, "generator.toml"))
 
-        for line in lines
-            if startswith(line, startswith_identifier)
-                @printf exports_file "export %s\n" split(line, r" |\(")[2]
-            end
-        end
-    end
-end
+for target in JLLEnvs.JLL_ENV_TRIPLES
+    @info "processing $target"
 
-# Generate export statements
-open(joinpath(SRC_DIR, "lC_exports_h.jl"), "w+") do exports_file
-    println(exports_file, "#   Generating exports")
+    options["general"]["output_file_path"] = joinpath(@__DIR__, "..", "lib", "$target.jl")
 
-    write_constants(joinpath(SRC_DIR, "lC_curl_h.jl"), "function", exports_file)
-    write_constants(joinpath(SRC_DIR, "lC_common_h.jl"), "const", exports_file)
+    args = get_default_args(target)
+    push!(args, "-I$include_dir")
 
-    # Generate define constants
-    open(joinpath(SRC_DIR, "lC_defines_h.jl"), "w+") do defines_file
-        println(defines_file, "#   Generating #define constants")
+    # header_files = detect_headers(include_dir, args)
+    header_files = [curl_h]
 
-        hashdefs = split(read(`gcc -E -dD -P $(joinpath(CURL_PATH, "curl.h"))`, String), "\n")
+    ctx = create_context(header_files, args, options)
 
-        for line in hashdefs
-            m = match(r"^\s*#define\s+CURL(\w+)\s+(.+)", line)
-
-            if m !== nothing
-                c2 = replace(m.captures[2], "(unsigned long)" => "")
-                @printf defines_file "const CURL%-30s = %s\n"  m.captures[1]  c2
-                @printf exports_file "export CURL%s\n"  m.captures[1]
-            end
-        end
-    end
+    build!(ctx)
 end
