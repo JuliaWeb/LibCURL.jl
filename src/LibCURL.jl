77a71382698e7811d9241dcd426075870ff11373
Gnimuc <qiyupei@gmail.com>
2021-05-19 13:40:46 +0900

    `Base.BUILD_TRIPLET` -> `Sys.MACHINE`

--- src/LibCURL.jl
+++ src/LibCURL.jl
@@ -14,7 +14,7 @@ function __init__()
     global cacert = MozillaCACerts_jll.cacert
 end
 
-const IS_LIBC_MUSL = occursin("musl", Base.BUILD_TRIPLET)
+const IS_LIBC_MUSL = occursin("musl", Sys.MACHINE)
 
 if Sys.isapple() && Sys.ARCH === :aarch64
     include("../lib/aarch64-apple-darwin20.jl")
@@ -45,7 +45,7 @@ elseif Sys.isbsd() && !Sys.isapple()
 elseif Sys.iswindows() && Sys.ARCH === :x86_64
     include("../lib/x86_64-w64-mingw32.jl")
 else
-    error("Unknown platform: $(Base.BUILD_TRIPLET)")
+    error("Unknown platform: $(Sys.MACHINE)")
 end
 
 # curl_share_setopt, curl_easy_getinfo, curl_easy_setopt, and curl_multi_setopt are vararg C functions
